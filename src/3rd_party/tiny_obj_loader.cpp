a7329060ab76919188090d142b908f4f0f083b9c
brndd <burneddi@gmail.com>
2023-02-12 20:29:40 +0200

    Add tinyobjloader.

---
+++ src/3rd_party/tiny_obj_loader.cpp
@@ -0,0 +1,5 @@
+//This macro enables the implementations of the tinyobjloader library
+//it has to be specified in EXACTLY ONE .cpp file. That's why it's specified in this dummy file that does nothing.
+#define TINYOBJLOADER_IMPLEMENTATION
+#define TINYOBJLOADER_USE_MAPBOX_EARCUT
+#include <tiny_obj_loader.h>
